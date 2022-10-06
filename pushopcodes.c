102dee0393698b24dc604280e38cc1832c20522a
Lulusha-XT <leulsileshi7@gmail.com>
2022-10-06 22:31:54 +0300

    pushopcodes.c

---
+++ pushopcodes.c
@@ -0,0 +1,37 @@
+#include "monty.h"
+/**
+* f_push - add node to the stack
+* @stack: stack head
+* @line_number: line_number
+* Return: no return
+*/
+void f_push(stack_t **stack, unsigned int line_number)
+{
+	int n, j = 0, flag = 0;
+
+	if (bus.arg)
+	{
+		if (bus.arg[0] == '-')
+			j++;
+		for (; bus.arg[j] != '\0'; j++)
+		{
+			if (bus.arg[j] > 57 || bus.arg[j] < 48)
+				flag = 1; }
+		if (flag == 1)
+		{ fprintf(stderr, "L%d: usage: push integer\n", line_number);
+			fclose(bus.file);
+			free(bus.content);
+			free_stack(*stack);
+			exit(EXIT_FAILURE); }}
+	else
+	{ fprintf(stderr, "L%d: usage: push integer\n", line_number);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*stack);
+		exit(EXIT_FAILURE); }
+	n = atoi(bus.arg);
+	if (bus.lifi == 0)
+		addnode(stack, n);
+	else
+		addqueue(stack, n);
+}
