c1b4be669a838e8c8817e0b413037b9abc5166ea
Lulusha-XT <leulsileshi7@gmail.com>
2022-10-06 22:40:17 +0300

    stackcalc2.c

---
+++ stackcalc2.c
@@ -0,0 +1,81 @@
+#include "monty.h"
+/**
+* f_div - divides the top two elements of the stack.
+* @stack: stack head
+* @line_number: line_number
+* Return: void
+*/
+void f_div(stack_t **stack, unsigned int line_number)
+{
+	stack_t *h;
+	int len = 0, aux;
+
+	h = *stack;
+	while (h)
+	{
+		h = h->next;
+		len++;
+	}
+	if (len < 2)
+	{
+		fprintf(stderr, "L%d: can't div, stack too short\n", line_number);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*stack);
+		exit(EXIT_FAILURE);
+	}
+	h = *stack;
+	if (h->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", line_number);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*stack);
+		exit(EXIT_FAILURE);
+	}
+	aux = h->next->n / h->n;
+	h->next->n = aux;
+	*stack = h->next;
+	free(h);
+}
+
+/**
+* f_mod - computes the modulus of the second element and the top element
+* of the stack
+* @stack: stack head
+* @line_number: line_number
+* Return: void
+*/
+void f_mod(stack_t **stack, unsigned int line_number)
+{
+	stack_t *h;
+	int len = 0, aux;
+
+	h = *stack;
+	while (h)
+	{
+		h = h->next;
+		len++;
+	}
+	if (len < 2)
+	{
+		fprintf(stderr, "L%d: can't mod, stack too short\n", line_number);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*stack);
+		exit(EXIT_FAILURE);
+	}
+	h = *stack;
+	if (h->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", line_number);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*stack);
+		exit(EXIT_FAILURE);
+	}
+	aux = h->next->n % h->n;
+	h->next->n = aux;
+	*stack = h->next;
+	free(h);
+}
